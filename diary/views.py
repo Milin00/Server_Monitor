ccb2d2833a9c6cf94c81bf6084fe7f8442d269b8
Milin00 <wtnbryt1122@gmail.com>
2026-06-20 19:08:58 +0900

    スーパーユーザー作成

--- diary/views.py
+++ diary/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import View
 from datetime import datetime
 from zoneinfo import ZoneInfo
+from .forms import PageForm
 
 class IndexView(View):
     def get(self, request):
@@ -9,5 +10,20 @@ class IndexView(View):
             ZoneInfo('Asia/Tokyo')
             ).strftime('%Y年-%m月-%d日 %H:%M:%S')
         return render(request, 'diary/index.html', {'datetime_now': datetime_now})
+
+
+class PageCreateView(View):
+    def get(self,request):
+        form = PageForm()
+        return render(request, 'diary/page_form.html', {'form': form})
     
+    def post(self, request):
+        form=PageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('diary:index')
+        return render(request, 'diary/page_form.html', {'form': form})
+
+            
 index = IndexView.as_view()
+page_create = PageCreateView.as_view()
